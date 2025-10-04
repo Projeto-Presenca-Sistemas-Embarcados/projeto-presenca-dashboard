6132101ee3abafd73e47751b45e2f93a8bdbe050
Diego Tasso <diegotassoneto@hotmail.com>
2025-10-04 18:10:28 -0300

    criando a base da estrutura do componente da barra lateral + css

---
+++ src/components/LateralBar/LateralBar.tsx
@@ -0,0 +1,32 @@
+import type { Dashboard } from '../../App'
+import './LateralBar.css'
+import avatar from '../../assets/avatar.png'
+
+interface Props {
+    active: Dashboard
+    onSelect: (d: Dashboard) => void
+}
+
+export default function LateralBar({ active, onSelect }: Props) {
+    return (
+        <aside className="sidebar">
+            <div className="sidebar-top">
+                <div className="avatar-circle" aria-hidden>
+                    <img src={avatar} alt="Avatar do usuário" />
+                </div>
+                <div className="sidebar-title">Minhas turmas</div>
+            </div>
+
+            <nav className="sidebar-nav">
+                <button className={`nav-btn ${active === 'portal' ? 'active' : ''}`} onClick={() => onSelect('portal')}>
+                    Portal
+                </button>
+
+
+                <button className={`nav-btn ${active === 'frequencia' ? 'active' : ''}`} onClick={() => onSelect('frequencia')}>
+                    Frequência dos Mexicanos
+                </button>
+            </nav>
+        </aside>
+    )
+}
